440acc87c0c713eefb3fe924aafc8660782e11a2
KrippaNandhini <79848429+KrippaNandhini@users.noreply.github.com>
2024-04-06 22:00:30 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -54,8 +54,9 @@ if uploaded_file is not None:
 # Get the class name from the dictionary
         # Find the index of the maximum value in the prediction array
         prediction_index = np.argmax(prediction)
+        if np.amax(prediction) < 0.05:
+            st.write("Submit a proper image")
 
-# Get the class name from the dictionary
-        class_name = list(class_dict.keys())[list(class_dict.values()).index(prediction_index)]
-
-        st.write(f"Predicted Class: {class_name}")
+        else:
+            class_name = list(class_dict.keys())[list(class_dict.values()).index(prediction_index)]
+            st.write(f"Predicted Class: {class_name}")
